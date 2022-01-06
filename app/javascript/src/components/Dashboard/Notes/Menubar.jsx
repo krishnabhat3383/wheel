7769140ee4f293374617174e59d1b9de5c3ff81c
Krishna Bhat <memotoskbhat@gmail.com>
2022-01-06 15:11:37 +0530

    Added menu bar

---
+++ app/javascript/src/components/Dashboard/Notes/Menubar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Search, Plus, Settings } from "neetoicons";
+import { Typography } from "neetoui/v2";
+import { MenuBar } from "neetoui/v2/layouts";
+
+const Menubar = () => {
+  return (
+    <div>
+      <MenuBar showMenu title="Notes">
+        <MenuBar.Block label="All" count={200} active />
+        <MenuBar.Block label="Users" count={80} />
+        <MenuBar.Block label="Leads" count={60} />
+        <MenuBar.Block label="Visitors" count={60} />
+        <MenuBar.SubTitle iconProps={[{ icon: Search }]}>
+          <Typography style="h5" textTransform="uppercase">
+            Segments
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Block label="Europe" count={80} />
+        <MenuBar.Block label="Middle-East" count={60} />
+        <MenuBar.Block label="Asia" count={60} />
+        <MenuBar.SubTitle
+          iconProps={[{ icon: Search }, { icon: Plus }, { icon: Settings }]}
+        >
+          <Typography style="h5" textTransform="uppercase">
+            Tags
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Block label="Sales" count={60} />
+        <MenuBar.Block label="Finance" count={60} />
+        <MenuBar.Block label="User Experience" count={60} />
+      </MenuBar>
+    </div>
+  );
+};
+
+export default Menubar;
