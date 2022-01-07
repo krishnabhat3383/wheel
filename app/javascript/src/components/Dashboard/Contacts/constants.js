85bddbbbc5d8fe20c691be0375445e4a84bff65f
Krishna Bhat <memotoskbhat@gmail.com>
2022-01-07 14:59:54 +0530

    Updated constants and Menubar

--- app/javascript/src/components/Dashboard/Contacts/constants.js
+++ app/javascript/src/components/Dashboard/Contacts/constants.js
@@ -1,8 +1,168 @@
 import React from "react";
 
-import { Typography, Avatar } from "neetoui/v2";
+import { Typography, Avatar } from "neetoui";
 
 export const CONTACTS = [
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Ronald Richards" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Ronald Richards
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Jacob Jones" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Jacob Jones
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Ronald Richards" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Ronald Richards
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Jacob Jones" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Jacob Jones
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Ronald Richards" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Ronald Richards
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Jacob Jones" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Jacob Jones
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Ronald Richards" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Ronald Richards
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
+  {
+    name: (
+      <div className="flex space-x-1 items-center">
+        <Avatar user={{ name: "Jacob Jones" }} />
+        <Typography style="h6" textTransform="uppercase">
+          Jacob Jones
+        </Typography>
+      </div>
+    ),
+    email: (
+      <Typography style="body2" className="text-gray-500">
+        albert@borer.com
+      </Typography>
+    ),
+    createdAt: (
+      <Typography style="body2" className="text-gray-500">
+        Feb, 5, 2021
+      </Typography>
+    ),
+  },
   {
     name: (
       <div className="flex space-x-1 items-center">
